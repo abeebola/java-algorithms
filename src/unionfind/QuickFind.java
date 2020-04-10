3e55aca1bb34fb5ffb565fd1cd0b2c308a2306a6
Abeeb Ola <abeebola@yahoo.com>
2020-04-10 17:17:27 +0100

    Extend UnionFind in QuickFind

--- src/unionfind/QuickFind.java
+++ src/unionfind/QuickFind.java
@@ -1,14 +1,9 @@
 package unionfind;
 
-public class QuickFind {
-    private int[] bucket;
+public class QuickFind extends UnionFind {
 
     public QuickFind(int N) {
-        bucket = new int[N];
-        // Initialise array values
-        for (int i = 0; i < bucket.length; i++) {
-            bucket[i] = i;
-        }
+        super(N);
     }
 
     public boolean connected(int p, int q) {
@@ -23,17 +18,5 @@ public class QuickFind {
             if (bucket[i] == pid)
                 bucket[i] = qid;
         }
-    }
-
-    @Override
-    public String toString() {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int value : bucket) {
-            stringBuilder.append(
-                    String.format("%s ", value)
-            );
-        }
-
-        return stringBuilder.toString().trim();
     }
 }
